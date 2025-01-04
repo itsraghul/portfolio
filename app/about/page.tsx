596c9f335613807bd929eee7fd87397466c94062
raghul.saravanan <raghul2521@gmail.com>
2025-01-04 20:43:03 +0530

    Skills details

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,47 @@
 import ResumeButton from '@/components/ResumeButton/ResumeButton'
-import { ArrowDown } from 'lucide-react'
+import { skills } from '@/constants/about'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
+
     return (
-        <div className='flex items-center justify-center gap-3'>
-            <span>
-                To know more about me, you can checkout the social links at below
-            </span>
-            <ArrowDown size={18} />
-            <span>
-                or through my
-            </span>
-            <ResumeButton fileLink={process.env.RESUME_LINK!} />
+        <div className='flex flex-col gap-2 mx-auto'>
+            <h1 className='text-2xl font-semibold font-mono  text-primary' >Skills</h1>
+            <RotatingSkills />
+            <div className='flex items-center justify-center gap-3'>
+                <span className='flex items-center font-mono'>
+                    To know more about me, you can checkout the social links at below or through
+                </span>
+                <ResumeButton fileLink={process.env.RESUME_LINK!} />
+            </div>
+
         </div>
     )
 }
 
-export default page
+export default page;
+
+
+
+const RotatingSkills = () => {
+    return <div className="skills-wrapper">
+        <div className="inner" style={{ "--quantity": skills.length } as React.CSSProperties}>
+            {skills.map((skill) => (
+                <div
+                    key={skill.index}
+                    className="skill-card"
+                    style={{
+                        "--index": skill.index,
+                        "--color-card": skill.color,
+                    } as React.CSSProperties}
+                >
+                    <div className="img flex items-center justify-center flex-col gap-2">
+                        <span className='font-mono text-sm font-semibold'>{skill.name}</span>
+                        <Image src={skill.logo} alt={`${skill.name}-logo`} width={80} height={80} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+}
---
+++ constants/about/index.ts
@@ -0,0 +1,15 @@
+import { Card } from "@/types";
+
+
+export const skills: Card[] = [
+    { index: 0, color: "255, 102, 0", name: "HTML", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
+    { index: 1, color: "0, 122, 204", name: "Typescript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" },
+    { index: 2, color: "255, 153, 51", name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" },
+    { index: 3, color: "0, 102, 204", name: "CSS", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" },
+    { index: 4, color: "204, 0, 0", name: "Redis", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redis/redis-original.svg" },
+    { index: 5, color: "72, 40, 120", name: "Playwright", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/playwright/playwright-original.svg" },
+    { index: 6, color: "252, 208, 142", name: "Junit", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/junit/junit-original.svg" },
+    { index: 7, color: "97, 218, 251", name: "React", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+    { index: 8, color: "200, 180, 142", name: "Redux", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" }
+
+];
