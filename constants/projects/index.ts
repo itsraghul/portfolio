c3e386be3760e3921335c9282440192e3f94aad6
raghul.saravanan <raghul2521@gmail.com>
2025-02-04 16:41:47 +0530

    Latest project and skills

--- constants/projects/index.ts
+++ constants/projects/index.ts
@@ -2,30 +2,37 @@ import { Project } from "@/types";
 
 export const PROJECTS: Project[] = [
     {
+        name: "Mock Data - Generate Realistic fake data",
+        stack: "Next.js | Faker.js | TailwindCSS",
+        description: "I built this platform to simplify the process of designing and generating mock data for developers, testers, and teams working on real-world applications. Whether you're building an app, testing APIs, or need sample datasets for your projects, this tool is here to make it fast, reliable, and fully customisable.The mock data generator currently supports 26 different data categories and allows you to create nearly 200+ unique types of data, catering to diverse application and testing needs. Currently supports generation of fake data in 43 different languages.",
+        githubLink: "",
+        websiteLink: "https://www.mock-data.com"
+    },
+    {
         name: "DataForge - Through WebScrape workflow",
         stack: "Next.js | Puppeteer | Prisma | OpenAI",
-        description: "A dataset creator through webscrape workflow. Currently use AI to extract data in html and send data through webhook. Can also schedule the Workflows to run appropriately.",
+        description: "A dataset creator for ML models through Web-scrape workflows. Allows users to create web-scraping workflows with various configuration to create web-scraping workflow to collect data for dataset. It has AI integration that allows AI to extract necessary selectors and data from website. Also has facilities to schedule the workflow to appropriate times using cron syntax. Currently can send the data through webhook. In future, In house dataset storage and marketplace for selling datasets will be implemented.",
         githubLink: "https://github.com/itsraghul/FlowBuilder",
         websiteLink: "https://flow-data-forge.vercel.app/"
     },
     {
         name: "Hearty-Foods - Food delivery app",
         stack: "Next.js | MongoDB | Cloudinary | Paypal API",
-        description: "A food order and delivery application that allows users to order food items and pay using paypal. The admin can manage the users and create new orders and live track delivery status and update them accordingly.",
+        description: "Hearty Foods is a food order and delivery site build using NextJS. It has MongoDB for data storage and Material UI for styling.User can be able to order food and see their order history.Payment can be made with cash or through Paypal.The Admin(owner) can create new dishes and administer the orders made by the users and check the total collection and all the data. It was build using several apis like JSCookies, Cloudinary, JWToken and so on for various features.",
         githubLink: "https://github.com/itsraghul/hearty-foods",
         websiteLink: "https://hearty-foods.vercel.app/"
     },
     {
         name: "MediNet - Decentralized Medical Network",
         stack: "Next.js | Ethereum | Web3 | IPFS",
-        description: "A Decentralised Platform that provides Patient Record Management Services and Medical Community Network for Doctors. Used IPFS for storing medical files in a secure decentralized way.",
+        description: "A Decentralized Platform that provides Patient Record Management Services and Medical Community Network for Doctors. Platform allows patient to have control over their medical record. And doctors to form their own medical networks for better patient diagnosis. The files shared in the platform are done using IPFS.",
         githubLink: "https://github.com/itsraghul/medi-net",
         websiteLink: "https://medinet.vercel.app/"
     },
     {
         name: "CampFund - Decentralized Funding Network",
         stack: "Next.js | Ethereum | Solidity | Azure",
-        description: "A Decentralised Platform that provides users to gather or donate funds to support projects over blockchain. It also has Azure Chat bot to help new users to get used to funding in web3.",
+        description: "An Open-Public Project Fund Raising Site. Uses Ethereum for exchange of funds. User can create their own project fund by deploying the smart contract instance with the help of this site. The Fund Raising Contract Factory have been deployed to Rinkeby Test Network. The Users need a Metamask Account and RinkebyEther to transfer and fund cryptocoins. A Project Fund manager can create a new request for projects and need the approval of contributors.All project changes are open and seen by the contributors which prevents embezzlement.Added Azure Chat bot for customer help.",
         githubLink: "https://github.com/itsraghul/camp-fund",
         websiteLink: "https://my-camp-fund.vercel.app/"
     },
