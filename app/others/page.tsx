351ccecbe1d4851ba56bdec2dcdd5fb7958a7c7f
raghul-velt <raghul@velt.dev>
2026-06-10 15:16:08 +0530

    chore: remove orphaned libs and ui leftovers, theme /others page
    Deletes lib/animations, lib/scrollUtils, sonner and ReadingProgress ui
    components (no remaining consumers); wraps /others in the ship world so it
    stays readable; roots eslint config for worktree checkouts; adds dev
    launch config.

--- app/others/page.tsx
+++ app/others/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import WorldShell from '@/components/bridge/WorldShell'
 
 const OthersPage = () => {
     return (
-        <div className='flex flex-col mx-auto items-center justify-center font-mono'>
+        <WorldShell world="ship" className='flex flex-col mx-auto items-center justify-center font-mono pt-24 pb-16 text-[var(--fg)]'>
             <h1 className='font-bold'>{"So, You've found the hidden page!"}</h1>
             <span> {"There's nothing much here. Just take this extra info about me."}</span>
             <h3 className='font-semibold'>My Top 7 Games</h3>
@@ -15,7 +16,7 @@ const OthersPage = () => {
                 <li>Spider Man</li>
                 <li>Ghost of Tsushima</li>
             </ul>
-        </div>
+        </WorldShell>
     )
 }
 
